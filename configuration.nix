b9af6d5bc94cd2f6a969f81170eadfc698e73e1c
mTd0t <leon.t.isip@gmail.com>
2025-08-30 19:03:51 +0800

    rawr

--- configuration.nix
+++ configuration.nix
@@ -109,6 +109,7 @@
   bc
   libnotify
   jetbrains-toolbox
+  nodejs_24
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   #  wget
   ];
