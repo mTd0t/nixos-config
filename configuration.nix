57fa6c19c336e911ce4e58052d268acdb0d42f34
Vince Leon I. Santos <118159966+mTd0t@users.noreply.github.com>
2025-10-01 16:24:47 +0800

    Add files via upload

--- configuration.nix
+++ configuration.nix
@@ -110,6 +110,7 @@
   libnotify
   jetbrains-toolbox
   nodejs_24
+  pkgs.discord 
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   #  wget
   ];
